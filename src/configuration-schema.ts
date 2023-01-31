1107859546155ebbec5f269de2c52f6bc82a5078
Muhammad Ahmad <muhammad.falak@staffbase.com>
2023-01-31 12:46:06 +0100

    CC/muhammad-countdown

---
+++ src/configuration-schema.ts
@@ -0,0 +1,105 @@
+/*!
+ * Copyright 2020, Staffbase GmbH and contributors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { UiSchema } from "@rjsf/core";
+import { JSONSchema7 } from "json-schema";
+
+/**
+ * schema used for generation of the configuration dialog
+ * see https://react-jsonschema-form.readthedocs.io/en/latest/ for documentation
+ */
+export const configurationSchema: JSONSchema7 = {
+  required: [
+    "title",
+    "countdowndate",
+    "expiredmessage"
+  ],
+  properties: {  
+    title: {
+      type: "string",
+      title: "Title"
+    },
+    showtitle: {
+      type: "boolean",
+      title: "Show Title"
+    },
+    titlecolor: {
+      type: "string",
+      title: "Title Color",
+      default: "#333333"
+    },
+    countdowndate: {
+      type: "string",
+      title: "Countdown Date + Time",
+      format: "date-time"
+    },
+    expiredmessage: {
+      type: "string",
+      title: "Expired Message",
+      default: "Countdown is done."
+    },
+    boxescolorbg: {
+      type: "string",
+      title: "Background Color",
+      default: "#FBC91E"
+    },
+    boxescolorborder: {
+      type: "string",
+      title: "Border Color",
+      default: "#FBC91E"
+    },
+    boxescolortext: {
+      type: "string",
+      title: "Text Color",
+      default: "#FFFFFF"
+    },
+  },
+};
+
+/**
+ * schema to add more customization to the form's look and feel
+ * @see https://react-jsonschema-form.readthedocs.io/en/latest/api-reference/uiSchema/
+ */
+export const uiSchema: UiSchema = {
+  title: {
+    "ui:autofocus": true,
+    "ui:emptyValue": "",
+    "ui:autocomplete": "off",
+    "ui:help": "Choose a title for the countdown."
+  },
+  showtitle: {
+    "ui:help": "Do you want to display the title?"
+  },
+  titlecolor: {
+    "ui:widget": "color",
+    "ui:help": "Choose a color for the title of the countdown. Default: #333333"
+  },
+  countdowndate: {
+    "ui:help": "Choose a date and time for the countdown."
+  },
+  expiredmessage: {
+    "ui:help": "Enter an expired message for the countdown."
+  },
+  boxescolorbg: {
+    "ui:widget": "color",
+    "ui:help": "Choose a color for the background of the countdown boxes. Default: #FBC91E"
+  },
+  boxescolorborder: {
+    "ui:widget": "color",
+    "ui:help": "Choose a color for the border of the countdown boxes. Default: #FBC91E"
+  },
+  boxescolortext: {
+    "ui:widget": "color",
+    "ui:help": "Choose a color for the text in the countdown boxes. Default: #FFFFFF"
+  },
+};
---
+++ src/countdown-widget.tsx
@@ -0,0 +1,119 @@
+/*!
+ * Copyright 2020, Staffbase GmbH and contributors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { count } from "console";
+import React, { ReactElement } from "react";
+import { BlockAttributes } from "widget-sdk";
+import CSS from "csstype";
+import * as ReactDOM from 'react-dom';
+
+/**
+ * React Component
+ */
+export interface CountdownWidgetProps extends BlockAttributes {
+  title: string;
+  showtitle: boolean;
+  titlecolor: string;
+  countdowndate: string;
+  expiredmessage: string;
+  boxescolorbg: string;
+  boxescolortext: string;
+  boxescolorborder: string;
+}
+
+export const CountdownWidget = ({title, showtitle, titlecolor, boxescolorbg, boxescolorborder, boxescolortext, countdowndate, expiredmessage  }: CountdownWidgetProps): ReactElement => {
+  
+  const targetDate = new Date(countdowndate);
+
+  function countdown() {
+    const currentDate = new Date();
+    const timeDiff = Number(targetDate) - Number(currentDate);
+  
+    const seconds = Math.floor((timeDiff / 1000) % 60);
+    const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+    const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  
+    return [timeDiff,days,hours,minutes,seconds];
+  }
+  
+  React.useEffect(()=>{
+    setInterval(()=>{countdown(),1000})
+  }
+  )
+  
+  const [timeDiff,days,hours,minutes,seconds] = countdown();
+
+
+  const titlecustomize: CSS.Properties = {
+    // textAlign: "center",
+    textSizeAdjust :boxescolortext,
+    color: titlecolor,
+    border:boxescolorborder, 
+    fontSize: "22.4px",
+  };
+
+  const countdowncustomize: CSS.Properties = {
+    // textAlign: "center",
+    textSizeAdjust :boxescolortext,
+    backgroundColor: boxescolorbg,
+    color: 'black',
+    border:boxescolorborder, 
+    fontSize: "22.4px",
+    paddingTop : "5px",
+  };
+
+  const textcustomize: CSS.Properties = {
+    // textAlign: "center",
+    paddingTop : "5px",
+    color: 'black',
+    borderRadius:"3px", 
+    fontSize: "16px",
+    display: "inline-block"
+  };
+
+
+  return <div>
+   { 
+    showtitle ? (
+    <div><span style= {titlecustomize}>{title} </span> </div>
+    ) :
+    <div> </div>
+   }
+      { 
+    timeDiff < 0? (
+      <div>{expiredmessage}</div>
+    ) :
+    <div>
+      <div style = {countdowncustomize}>
+        <div>{days}</div>
+        <span style={textcustomize}>Days</span>
+      </div>  
+      <div style = {countdowncustomize}>
+        <div>{hours}</div>
+        <span style={textcustomize}>Hours</span>
+      </div>
+      <div style = {countdowncustomize}>
+        <div>{minutes}</div>
+        <span style={textcustomize}>Minutes</span>
+      </div>
+      <div style = {countdowncustomize}>
+        <div>{seconds}</div>
+        <span style={textcustomize}>Seconds</span>
+      </div>     
+    </div>
+   }
+   
+    </div>;
+};
+
